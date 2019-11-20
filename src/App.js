dff0215c315eb47973aedd60fb2771831e7638ef
huynhsontung <mrhuynhsontung@gmail.com>
2019-11-20 11:26:13 +0700

    Bo sung folder  Component config HOC Layouts Redux Screens Service Templates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React, { Component } from "react";
 import HomeScreen from "./Screens/Home/home";
-import CourseDetail from "./Screens/CourseDetail/courseDetail";
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Header from "./Layouts/Header";
-import HocDemo from "./HOC/HocDemo";
-import Login from "./Screens/Login/Login";
-import { connect } from "react-redux";
-import reduxAction from "./Redux/Action/action";
-import { LOGIN } from "./Redux/Action/type";
-import { restConnector } from "./Services";
-import { settings } from "./config/settings";
-import PrivateRoute from "./HOC/Auth";
-import SignupComponent from "./Screens/Signup/signup"
+
 
 
 class App extends Component {
@@ -20,14 +11,9 @@ class App extends Component {
       <BrowserRouter>
         <Header />
         <Switch>
-          {/* <Route path='/home' Component={HomeScreen} /> */}
-          <PrivateRoute path='/home' Component={HomeScreen}/>
-          <Route path='/coursedetail/:courseid' component={CourseDetail} />
-          <PrivateRoute path='/demohoc' Component={HocDemo} />
-          <Route path='/login' component={Login} />
-          <Route path='/signup' component={SignupComponent}/>>
+          <Route path='/home' Component={HomeScreen}/>
 
-          <PrivateRoute path='/' Component={HomeScreen} />
+          <Route path='/' Component={HomeScreen} />
         </Switch>
 
       </BrowserRouter>
