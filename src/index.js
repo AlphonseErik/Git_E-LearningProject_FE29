87062307224eb852b972dcaa5323a7c150ae9da1
huynhsontung <mrhuynhsontung@gmail.com>
2019-11-20 11:22:06 +0700

    Sua index va App

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import RootReducer from "./Redux/Reducer/rootReducer";
+import reduxThunk from 'redux-thunk';
+
+const composeEnhancers =
+    typeof window === 'object' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+            // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+        }) : compose;
+
+const enhancer = composeEnhancers(
+    applyMiddleware(reduxThunk),
+    // other store enhancers if any
+);
+
+const store = createStore(
+    RootReducer, enhancer
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
