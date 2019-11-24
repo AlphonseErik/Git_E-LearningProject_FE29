e5dab3fab24e9e110bc0f2fcf16d0fad8d974614
huynhsontung <mrhuynhsontung@gmail.com>
2019-11-24 10:24:20 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from "react-redux";
+import Header from "./Layouts/Header/header";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Header/>
         <Switch>
         </Switch>
 
---
+++ src/Layouts/Header/header.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+                <a className="navbar-brand" href="#">E-Learning</a>
+                <button className="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation" />
+                <div className="collapse navbar-collapse" id="collapsibleNavId">
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home<span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+        )
+    }
+}
+
+export default Header;
