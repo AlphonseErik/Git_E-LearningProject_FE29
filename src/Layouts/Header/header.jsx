52c8d928f9e34b78578a9a3de254d2f4976ce5db
huynhsontung <mrhuynhsontung@gmail.com>
2019-11-25 11:34:30 +0700

    sua header

--- src/Layouts/Header/header.jsx
+++ src/Layouts/Header/header.jsx
@@ -13,39 +13,36 @@ class Header extends Component {
                             <a className="nav-link" href="#"><i className="fa fa-th mr-2"></i>Categories<span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item active">
-                        <div className="input-group ml-3">
-                            <input type="text" className="form-control" placeholder="Search for anything" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                            <div className="input-group-append">
-                                <span className="input-group-text" id="basic-addon2"><i className="fa fa-search"></i></span>
-                            </div>
+                            <div className="input-group ml-3">
+                                <input type="text" className="form-control" placeholder="Search for anything" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                                <div className="input-group-append">
+                                    <span className="input-group-text" id="basic-addon2"><i className="fa fa-search"></i></span>
+                                </div>
                             </div>
                         </li>
-                        
+
                     </ul>
-                
-                    
-                    <div className=" mr-3">
-                        <a className="nav-link text-white" href="#"> udemy for Business</a>
-                    </div>
-                    <div >
-                        <a className="nav-link text-white" href="#">Tech on Udemy</a>
-                    </div>
-                  
-                    <ul  className="navbar-nav ml-auto mt-2 mt-lg-0">
-                    <div className="mr-3 pt-2 ">
-                        <a href="#">
-                       <i className="text-white fa fa-shopping-cart"></i>
-                       </a>
-                   </div>
-                        <li className="nav-item active">
-                            <button className="btn bg-white">
-                            <a className="text-dark" href="#">
-                                Login
-                                </a>
-                            </button>
-                           
+                    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+                        <li className="nav-item">
+                            <div className=" mr-4">
+                                <a className="nav-link text-white" href="#"> udemy for Business</a>
+                            </div>
                         </li>
-                    
+                        <li className="nav-item">
+                            <div className=" mr-3">
+                                <a className="nav-link text-white" href="#">Tech on Udemy</a>
+                            </div>
+                        </li>
+                        <li className="nav-item" >
+                            <div className="mr-3 pt-2 ">
+                                <a href="#">
+                                    <i className="text-white fa fa-shopping-cart"></i>
+                                </a>
+                            </div>
+                        </li>
+                        <li className="nav-item active">
+                            <a className="text-light" href="#" className="btn bg-danger">Login</a>
+                        </li>
                         <li className="nav-item active">
                             <a className="nav-link" href="#">Sign Up</a>
                         </li>
