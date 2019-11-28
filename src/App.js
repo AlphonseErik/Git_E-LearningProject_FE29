4878dbd8fe2eee8d20b140dcce1ba4747db53052
huynhsontung <mrhuynhsontung@gmail.com>
2019-11-28 16:59:42 +0700

    Them guard vao Project, can dang nhap de xu dung

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from "react-redux";
 import Header from "./Layouts/Header/header";
 import HomeScreen from "./Screens/Home/home";
-import SideBar from "./Layouts/SideBar/SideBar";
-import BottomSideBar from "./Layouts/SideBar/BottomSideBar";
 import { restConnector } from "./Services";
 import reduxAction from "./Redux/Action/action";
 import { LOGIN } from "./Redux/Action/actionType";
 import UserProfile from "./Screens/UserProfile/userProfile";
 import Login from "./Screens/Login/login";
 import Signup from "./Screens/Signup/signup";
+import PrivateRoute from "./HOC/Auth"
 
 class App extends Component {
   render() {
@@ -21,7 +20,7 @@ class App extends Component {
         <Switch>
         {/* <HomeScreen /> */}
           <Route path="/home" exact component={HomeScreen}/>
-          <Route path="/userprofile" exact component={UserProfile}/>
+          <PrivateRoute path="/userprofile" Component={UserProfile}/>
           <Route path="/login" exact component={Login}/>
           <Route path="/signup" exact component={Signup}/>
 
