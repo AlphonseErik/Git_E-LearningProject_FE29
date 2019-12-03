4adbda07961b3b5a1608f1661970c2cb16ea23e5
gia han ly <hanjimj000@gmail.com>
2019-12-04 01:57:42 +0700

    ...

--- src/Components/CourseItem/courseItem.jsx
+++ src/Components/CourseItem/courseItem.jsx
@@ -1,25 +1,45 @@
 import React, { Component } from "react";
 import classes from "./courseItemStyle.module.scss";
-import { Button } from "@material-ui/core";
+import Popup from 'reactjs-popup';
+
 
 class CourseItem extends Component {
   render() {
-    const { hinhAnh, tenKhoaHoc, moTa, luotXem } = this.props.item;
-
+    const { hinhAnh, tenKhoaHoc, moTa, luotXem,ngayTao } = this.props.item;
+   
     return (
-      <div className="container">
-        <div className={classes.courseItem}>
-          <div className={classes.card}>
-            <img className="card-img-top" src={hinhAnh} />
-            <div className="card-body text-left">
-              <p className="card-title">{this._shortenStringTenKhoaHoc(tenKhoaHoc)}</p>
-              <span className="card-text">{this._shortenString(moTa)}</span>
-              <h5 className="mt-1">Lượt xem: {luotXem}</h5>
-              <button className="btn btn-success">Thêm chi tiết</button>
-            </div>
+      <Popup  trigger={  <div className="container" >
+      <div className={classes.courseItem}>
+        <div className={classes.card}>
+          <img className="card-img-top" src={hinhAnh} />
+          <div className="card-body text-left">
+            <p className="card-title">{this._shortenStringTenKhoaHoc(tenKhoaHoc)}</p>
+            <span className="card-text">{this._shortenString(moTa)}</span>
+            <h5 className="mt-1">Lượt xem: {luotXem}</h5>
+            <button className="btn btn-success">Thêm chi tiết</button>
+            
           </div>
         </div>
       </div>
+    </div>} position={["top center", "bottom right", "bottom left"]} on={"hover"}>
+      <div  style={{height:300,width:300}}>
+    <div className={classes.coursePopup}>
+        <div className={classes.cardPopup}>
+          <img className="card-img-top" src={hinhAnh} />
+          <div className="card-body text-left">
+            <p className="card-title">{tenKhoaHoc}</p>
+            <span className="card-text">{moTa}</span>
+            <h5 className="mt-1">Lượt xem: {luotXem}</h5>
+            <button className="btn btn-success">Thêm chi tiết</button>
+            
+          </div>
+        </div>
+      </div>
+      </div>
+           </Popup>
+           
+    
+    
     )
   }
 
