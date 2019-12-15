309d870a08dfef4b893171408bc0a12aa578a9d7
huynhsontung <mrhuynhsontung@gmail.com>
2019-12-15 16:48:02 +0700

    fix loi

--- src/Components/AdminProfileScreen/TableOfUser/AcceptCourseForUser/acceptCourseForUser.jsx
+++ src/Components/AdminProfileScreen/TableOfUser/AcceptCourseForUser/acceptCourseForUser.jsx
@@ -6,9 +6,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { green } from '@material-ui/core/colors';
 import Button from '@material-ui/core/Button';
-import Fab from '@material-ui/core/Fab';
-import CheckIcon from '@material-ui/icons/Check';
-import SaveIcon from '@material-ui/icons/Save';
 
 const useStyles = makeStyles(theme => ({
     root: {
